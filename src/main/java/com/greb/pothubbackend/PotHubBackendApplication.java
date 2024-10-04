bc3e2448cd934becf14f2c00f199550035f97f14
HungTran170904 <tienhung17092004@gmail.com>
2024-10-04 22:04:40 +0700

    feat: init project

---
+++ src/main/java/com/greb/pothubbackend/PotHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.greb.pothubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotHubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greb/pothubbackend/PotHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greb.pothubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
